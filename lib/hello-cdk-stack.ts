a950eeff7f511f98105a82126faf162a66aece30
Kandie Kip <hkandie@sourceallies.com>
2025-04-28 14:30:31 -0500

    chore: lint

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -7,28 +7,24 @@ import * as iam from "aws-cdk-lib/aws-iam";
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    const addThingToGroupFunction = new lambda.Function(
-      this,
-      "sample-lambda",
-      {
-        runtime: lambda.Runtime.PYTHON_3_10,
-        code: lambda.Code.fromAsset("lambda"),
-        handler: "index.handler",
-        functionName: "sample-lambda",
-        environment: {
-          KINESIS_STREAM_REGION: this.region,
-          ACCOUNT_ID: this.account,
-        },
-        role: new iam.Role(this, "lambda-role", {
-          assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
-          managedPolicies: [
-            iam.ManagedPolicy.fromAwsManagedPolicyName(
-              "service-role/AWSLambdaBasicExecutionRole"
-            ),
-          ],
-        }),
-      }
-    );
+    const addThingToGroupFunction = new lambda.Function(this, "sample-lambda", {
+      runtime: lambda.Runtime.PYTHON_3_10,
+      code: lambda.Code.fromAsset("lambda"),
+      handler: "index.handler",
+      functionName: "sample-lambda",
+      environment: {
+        KINESIS_STREAM_REGION: this.region,
+        ACCOUNT_ID: this.account,
+      },
+      role: new iam.Role(this, "lambda-role", {
+        assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+        managedPolicies: [
+          iam.ManagedPolicy.fromAwsManagedPolicyName(
+            "service-role/AWSLambdaBasicExecutionRole",
+          ),
+        ],
+      }),
+    });
     // The code that defines your stack goes here
 
     // example resource
